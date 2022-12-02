f3a17af5eb927d430728e70174cbcb4eebf1aec5
TED <teddy5518@hotmail.com>
2022-12-03 02:06:00 +0900

    [programmers] Lv.0_두수의합

---
+++ Programmers/Lv.0두수의합.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0두수의합.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120802(_ num1:Int, _ num2:Int) -> Int {
+    // Addition
+    return num1 + num2
+}
