f4fec6b173a8d79f19ed6539a2d2d3139e1e960d
TED <teddy5518@hotmail.com>
2022-12-03 02:00:17 +0900

    [programmers] Lv.0_나머지구하기

---
+++ Programmers/Lv.0나머지구하기.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0나머지구하기.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120810(_ num1:Int, _ num2:Int) -> Int {
+    // Remainder Operator
+    return num1 % num2
+}
