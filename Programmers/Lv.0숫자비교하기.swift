4bbc0a79ac62140454634d2361034a0cea0db059
TED <teddy5518@hotmail.com>
2022-12-03 01:45:14 +0900

    [programmers] Lv.0_숫자비교하기

---
+++ Programmers/Lv.0숫자비교하기.swift
@@ -0,0 +1,17 @@
+//
+//  Lv.0숫자비교하기.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120807(_ num1:Int, _ num2:Int) -> Int {
+    if num1 == num2 {
+        return 1
+    }
+    else {
+        return -1
+    }
+}
