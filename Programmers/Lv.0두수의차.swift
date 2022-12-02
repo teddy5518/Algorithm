e3775ce64e2128dce1c267990fc5c4047ca04da1
TED <teddy5518@hotmail.com>
2022-12-03 01:39:55 +0900

    [programmers] Lv.0_두수의차

---
+++ Programmers/Lv.0두수의차.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0두수의차.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120803(_ num1:Int, _ num2:Int) -> Int {
+// to subtract
+    return num1 - num2
+}
