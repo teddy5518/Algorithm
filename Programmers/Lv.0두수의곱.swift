ac72417932966f0a7c7d756e510d222418df0d2a
TED <teddy5518@hotmail.com>
2022-12-03 01:43:16 +0900

    [programmers] Lv.0_두수의곱

---
+++ Programmers/Lv.0두수의곱.swift
@@ -0,0 +1,12 @@
+//
+//  Lv.0두수의곱.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120804(_ num1:Int, _ num2:Int) -> Int {
+    return num1*num2
+}
