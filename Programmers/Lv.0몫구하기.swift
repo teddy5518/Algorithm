b6e3658ef796b76243c5f51f09b5aeff7d788022
TED <teddy5518@hotmail.com>
2022-12-03 01:35:17 +0900

    [programmers] Lv.0_몫구하기

---
+++ Programmers/Lv.0몫구하기.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0몫구하기.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/3/22.
+//
+
+import Foundation
+
+func solution120805(_ num1:Int, _ num2:Int) -> Int {
+    // to divide
+    return num1/num2
+}
