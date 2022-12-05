069fdd3dccf1ed00dc11bbc5f600b16a80b09c56
TED <teddy5518@hotmail.com>
2022-12-06 01:40:15 +0900

    [programmers] Lv.0_배열뒤집기

---
+++ Programmers/Lv.0배열뒤집기.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0배열뒤집기.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/6/22.
+//
+
+import Foundation
+
+func solution(_ num_list:[Int]) -> [Int] {
+    // reversed() returns a view presenting the elements of the collection in reverse order
+    return num_list.reversed()
+}
