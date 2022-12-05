3a1bc6cf902d3ab45a9f47188a72cb40b0212ddb
TED <teddy5518@hotmail.com>
2022-12-06 01:57:51 +0900

    [programmers] Lv.0_문자열뒤집기

---
+++ Programmers/Lv.0문자열뒤집기.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0문자열뒤집기.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/6/22.
+//
+
+import Foundation
+
+func solution120822(_ my_string:String) -> String {
+    // used reversed() then created a new string out of the result
+    return String(my_string.reversed())
+}
