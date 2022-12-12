7a010934fe4d27a9372ad397d189d4c3bb855007
TED <teddy5518@hotmail.com>
2022-12-13 03:05:09 +0900

    [programmers] Lv.0_편지

---
+++ Programmers/Lv.0편지.swift
@@ -0,0 +1,13 @@
+//
+//  Lv.0편지.swift
+//  Programmers
+//
+//  Created by TEDDY on 12/13/22.
+//
+
+import Foundation
+
+func solution120898(_ message:String) -> Int {
+    // counts characters in message then multiply 2 to figure the answer
+    return message.count * 2
+}
